7ca570fd417c02daa436296dc48d23d9f207f390
Yurii Timofeev <40800136+georgius1024@users.noreply.github.com>
2019-02-16 18:31:20 +0500

    Uploaded build

---
+++ precache-manifest.b850f0aac37df858435a9f516e9209dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7b78de03796df2fe63f0b1d247cf21",
+    "url": "/static/media/Weather-PNG-File.3d7b78de.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e888d76986e6cb792b23",
+    "url": "/static/js/main.e888d769.chunk.js"
+  },
+  {
+    "revision": "1c89be9b5190dd83bb7a",
+    "url": "/static/js/1.1c89be9b.chunk.js"
+  },
+  {
+    "revision": "e888d76986e6cb792b23",
+    "url": "/static/css/main.7e1f4cb1.chunk.css"
+  },
+  {
+    "revision": "1c89be9b5190dd83bb7a",
+    "url": "/static/css/1.bfe95897.chunk.css"
+  },
+  {
+    "revision": "2297ce01949a07747c56aecb47959bf1",
+    "url": "/index.html"
+  }
+];
